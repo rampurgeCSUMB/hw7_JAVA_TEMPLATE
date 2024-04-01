f5376161c8de7417f6b81d687a88ffb5a344e193
rampurgeCSUMB <98712477+rampurgeCSUMB@users.noreply.github.com>
2024-04-01 11:13:37 -0700

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+ * INSTRUCTION:
+ *     This is a Java starting code for hw0_1.
+ *     When you finish the development, commit and push the file to github.
+ */
+
+// Finish the head comment with Abstract, ID, Name, and Date.
+/*
+ * Title: Main_hw0_1.java
+ * Abstract: Write the main purpose of the program.
+ * Name: Write your name
+ * Date: MM/DD/YYYY
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        /*
+         * Add your code here
+         */
+    }
+}
---
+++ test2.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [[ -f "./Main.java" ]]; then
+  javac Main.java
+  if [[ $? -ne 0 ]]; then  # Check for compilation errors
+    echo "Compilation failed"
+    exit 1
+  fi
+
+  passed=0
+  failed=0
+  total=0
+
+  if [[ -d "test2/input" ]] && [[ -d "test2/output" ]]; then
+    for input in test2/input/*.txt; do
+      ((total++))
+      base_name=$(basename "$input" .txt)
+      output_file="test2/output/${base_name}.txt"
+      java Main < "$input" > temp.txt
+      if [[ $? -ne 0 ]]; then  # Check for execution errors
+        echo "Execution failed for $input"
+        ((failed++))
+        continue
+      fi
+
+      if diff -qwB temp.txt "$output_file"; then
+        echo "Test Passed: Output matches expected output for $base_name"
+        ((passed++))
+      else
+        echo -e "Test Failed: Output does not match expected output for $base_name\n"
+        echo -e "\tActual output:\n"
+        ((failed++))
+        cat temp.txt
+        echo -e "\n\tExpected output:\n"
+        cat "$output_file"
+        echo ""
+      fi
+    done
+  else
+    echo "Error: Directories 'test2/input' or 'test2/output' not found."
+    exit 1
+  fi
+  echo "-------------------------------"
+  echo "$passed passed $failed failed"
+  echo "-------------------------------"
+  rm temp.txt
+else
+  echo "Coudn't find Main.java"
+  exit 1
+fi
